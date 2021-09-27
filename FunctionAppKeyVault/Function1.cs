cad516835af40c112fd073c53a4128b29a8f1c69
Ícaro <icaro.almeida@vaivoa.com>
2021-09-27 14:17:09 -0300

    Add project files.

---
+++ FunctionAppKeyVault/Function1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionAppKeyVault
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string kv_SecUser01 = Environment.GetEnvironmentVariable($"kv_SecUser01");
+            string kv_SecPass01 = Environment.GetEnvironmentVariable($"kv_SecPass01");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            responseMessage = "kv_SecUser01: " + kv_SecUser01 + "kv_SecPass01: " + kv_SecPass01;
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
